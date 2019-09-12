cfa4c016ac91248f19f4e596e06408cee17ebbe4
meloddity <53349597+meloddity@users.noreply.github.com>
2019-09-12 13:49:21 -0600

    Color Changer Exercise

---
+++ ColorChanger.js
@@ -0,0 +1,30 @@
+function redBtn() {
+	document.getElementById("box").style.background = "red";
+}
+function orangeBtn() {
+	document.getElementById("box").style.background = "orange";
+}
+function yellowBtn() {
+	document.getElementById("box").style.background = "yellow";
+}
+function greenBtn() {
+	document.getElementById("box").style.background = "green";
+}
+function blueBtn() {
+	document.getElementById("box").style.background = "blue";
+}
+function purpleBtn() {
+	document.getElementById("box").style.background = "purple";
+}
+function whiteBtn() {
+	document.getElementById("box").style.background = "white";
+}
+function blackBtn() {
+	document.getElementById("box").style.background = "black";
+}
+function grayBtn() {
+	document.getElementById("box").style.background = "gray";
+}
+function brownBtn() {
+	document.getElementById("box").style.background = "#BF926B";
+}
